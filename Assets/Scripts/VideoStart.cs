606b9ec34025b29169595dacad717fe9ffbecf6b
Jou Okuyama <ojoxux@gmail.com>
2024-06-28 18:19:52 +0900

    style: コメントを追加

--- Assets/Scripts/VideoStart.cs
+++ Assets/Scripts/VideoStart.cs
@@ -8,23 +8,19 @@ public class VideoEndTransition : MonoBehaviour
 
     void Start()
     {
-        // VideoPlayer�R���|�[�l���g���擾
+        // Get the VideoPlayer component
         videoPlayer = GetComponent<VideoPlayer>();
-
-        // ����t�@�C����ݒ�
         videoPlayer.url = Application.dataPath + "/Movie/レコーディング 2024-06-17 140710.mp4";
-
-        // ����Đ����J�n
+        // Play the video
         videoPlayer.Play();
     }
 
     void Update()
     {
-        // ����̍Đ����I�����������`�F�b�N
+        // Check if the video has ended
         if (videoPlayer.frame > 0 && videoPlayer.frame == (long)videoPlayer.frameCount - 1)
         {
             Debug.Log("Video has ended. Transitioning to the Start scene.");
-            // Start�V�[���ɑJ��
             SceneManager.LoadScene("Start");
         }
     }
