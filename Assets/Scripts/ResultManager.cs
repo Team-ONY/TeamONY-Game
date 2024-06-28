6cc05b501f5343f1622435a26a27e288c073c46f
Jou Okuyama <ojoxux@gmail.com>
2024-06-28 10:58:10 +0900

    feat: リザルト画面に表示するテキストを制御するResultManager.csの実装

---
+++ Assets/Scripts/ResultManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour
+{
+    public TMP_Text lastQuestionText;
+    public TMP_Text lastCorrectAnswerText;
+    public TMP_Text explanationText;
+    public TMP_Text scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lastQuestionText.text = "間違った問題: " + PlayerPrefs.GetString("LastQuestion","");
+        lastCorrectAnswerText.text = "正解: " + PlayerPrefs.GetString("LastCorrectAnswer","");
+        explanationText.text = "解説: " + PlayerPrefs.GetString("LastExplanation","");
+        scoreText.text = "正解数: " + PlayerPrefs.GetInt("CorrectCount",0).ToString();
+    }
+}
