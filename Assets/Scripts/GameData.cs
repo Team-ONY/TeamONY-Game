a97ba2985bf397e06121f424723a06f862a71c0a
Jou Okuyama <ojoxux@gmail.com>
2024-08-28 10:37:29 +0900

    feat: Pass unanswered question and answers to Game Over screen

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,7 @@
+public static class GameData
+{
+    public static int CorrectAnswers { get; set; }
+    public static string LastQuestion { get; set; }
+    public static string LastCorrectAnswer { get; set; }
+    public static string LastExplanation { get; set; }
+}
